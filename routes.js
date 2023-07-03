9ee093c71620d18b4777184b1a8c6b657aecb384
johannesdeutsch <johannes.deutsch@gmx.at>
2023-07-03 17:02:48 +0200

    user routes and some course routes

--- routes.js
+++ routes.js
@@ -12,5 +12,29 @@ router.get('/users', authenticateUser, asyncHandler(async (req, res) => {
     res.json(user);
 }));
 
+router.post('/users', authenticateUser, asyncHandler(async (req, res) => {
+    const newUser = await User.create(req.body);
+    res.location('/');
+    res.status(201).json(newUser);
+}));
+
+//Retrieve a collection of all courses and a 200 HTTP status code
+router.get('/courses', authenticateUser, asyncHandler(async (req, res) => {
+    const courses = await Course.findAll();
+    res.json(courses);
+}));
+
+router.get('/courses/:id', authenticateUser, asyncHandler(async (req, res) => {
+    const userCourse = await Course.findByPk(req.params.id);
+    res.json(userCourse);
+}));
+
+router.post('/courses', authenticateUser, asyncHandler(async (req, res) => {
+    const newCourse = await Course.create(req.body);
+    console.log(req.body);
+    console.log(res);
+    res.location(`/courses/${newCourse.userId}`);
+    res.status(201).json(newCourse);
+}));
 
 module.exports = router;
